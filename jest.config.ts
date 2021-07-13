4def04b049662b1d94010751b7cc8eba4b4dc18b
calebmsword <caleb.m.sword@gmail.com>
2021-07-12 21:27:48 -0400

    initial commit

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import { withEnzyme } from 'jest-expo-enzyme';
+import {defaults} from 'jest-config';
+
+module.exports = {
+    moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+    projects: [
+      // Skipping Node because we want to test DOM presets only
+      withEnzyme(require('jest-expo/ios/jest-preset')),
+      withEnzyme(require('jest-expo/android/jest-preset')),
+      // The Enzyme support added to web is different from that added to native, which `withEnzyme` handles
+      // Luckily you won't have to do anything special because it reads the platform from
+      // `haste.defaultPlatform` of the provided Jest config
+      withEnzyme(require('jest-expo/web/jest-preset')),
+    ],
+    collectCoverageFrom: [
+        "./src/**/*.{ts, tsx}",
+        // "!**/node_modules/**",
+    ],
+    coverageDirectory: 'coverage',
+  };
