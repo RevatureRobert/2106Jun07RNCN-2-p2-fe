58d9695022fbc7e0cbb49719dcbba7542dad2de9
Red Oral <redmunozoral@gmail.com>
2021-07-13 09:59:37 -0700

    Added AddChirpView

--- src/components/AddChirpView.tsx
+++ src/components/AddChirpView.tsx
@@ -6,30 +6,45 @@ const AddChirpView: React.FC = () => {
 
     return (
         <View style={styles.AddChirpViewContainer}>
-            <Image source={require('../assets/defaultUserImage.png')} style={{width: 64, height: 64, borderRadius: 72/2}}></Image>
-            <TextInput
-                style={styles.input}
-                onChangeText={inputState => { setInputState(inputState) }}
-                value={inputState}
-            />
+            <View style={styles.AddChirpContent}>
+                <Image source={require('../assets/defaultUserImage.png')} style={{width: 64, height: 64, borderRadius: 72/2}}></Image>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={inputState => { setInputState(inputState) }}
+                    value={inputState}
+                />
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-   AddChirpViewContainer: {
-    backgroundColor: '#111',
-    color: '#fff',
-    flex: 1,
-    flexDirection: 'row'
-   },
+    AddChirpViewContainer: {
+        backgroundColor: '#111',
+        color: '#fff',
+        flex: 1,
+        flexDirection: 'column',
+       },
+
+    AddChirpContent: {
+        backgroundColor: '#111',
+        color: '#fff',
+        flex: 1,
+        flexDirection: 'row',
+        padding: 25
+    },
 
    input: {
     color: '#fff',
-    height:50,
+    height:256,
     padding: 25,
-    borderWidth: 2,
-    borderColor: '#fff'
+    borderWidth: 1,
+    marginLeft: 12,
+    marginTop: 6,
+    borderColor: '#333',
+    textAlign: 'left',
+    textAlignVertical: 'top',
+    flex: 1
    }
 });
 
