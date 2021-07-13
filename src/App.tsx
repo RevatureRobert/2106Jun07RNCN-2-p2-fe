ce5fa61e00877222736b9218463e24cdc70791f9
Dague Fleurantin <dague04@gmail.com>
2021-07-13 11:08:18 -0400

    structuring app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
-import { Store } from './components/Redux/store/store';
+import { Store } from './Redux/store/store';
 
 export default function App() {
   return (
--- src/Redux/actions/ChirpActions.ts
+++ src/Redux/actions/ChirpActions.ts
@@ -1,4 +1,4 @@
-import axios from '../../axiosConfig';
+import axios from '../../components/axiosConfig';
 import { Dispatch } from 'redux';
 import {
   ChirpsActionsTypes,
