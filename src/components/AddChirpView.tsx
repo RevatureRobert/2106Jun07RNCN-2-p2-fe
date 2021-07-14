5c1e17de6ed76267f205edf8150f582b78791fbb
marcski55 <marc.skwarczynski@revature.net>
2021-07-14 14:33:51 -0700

    Removed unnecessary parameter from AddChirpView

--- src/components/AddChirpView.tsx
+++ src/components/AddChirpView.tsx
@@ -24,7 +24,7 @@ const AddChirpView: React.FC = () => {
           style={styles.input}
           placeholder='Posting as @redoral'
           placeholderTextColor='#dfdfdf'
-          onChangeText={(inputState) => {
+          onChangeText={() => {
             setInputState(inputState);
           }}
           value={inputState}
