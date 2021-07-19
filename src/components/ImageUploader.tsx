434fa10ed98ffaf93a4530a47bebaccb7895d41b
marcski55 <marc.skwarczynski@revature.net>
2021-07-19 09:20:58 -0700

    Worked on imageUploader

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { View, Platform, TouchableOpacity } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { config } from 'dotenv';
+config();
+
+const client = new S3Client({});
 
 export const ImageUploader = () => {
   const [image, setImage] = useState('');
+  const [percentage, setPercentage] = useState(0);
 
   useEffect(() => {
     (async () => {
@@ -30,6 +36,19 @@ export const ImageUploader = () => {
 
     if (!result.cancelled) {
       setImage(result.uri);
+      return uploadImage();
+    }
+
+    return 'Error';
+  };
+
+  const uploadImage = async () => {
+    // https://github.com/expo/examples/blob/master/with-aws-storage-upload/App.js
+    try {
+      //upload code here
+    } catch (e) {
+      console.log(e);
+      alert('Upload failed');
     }
   };
 
