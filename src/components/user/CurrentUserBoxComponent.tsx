672c03f10aaf34e20be426b488b355d07e87fb4b
Red Oral <redmunozoral@gmail.com>
2021-07-19 11:17:32 -0700

    Added profile picture for CurrentUserBoxComponent

--- src/components/user/CurrentUserBoxComponent.tsx
+++ src/components/user/CurrentUserBoxComponent.tsx
@@ -4,17 +4,19 @@ import {
   View,
   Image,
   Text,
-  TouchableHighlight
+  TouchableHighlight,
 } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootStore } from '../../redux/store/store';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { logout } from '../../redux/actions/AuthActions';
+import { Storage } from 'aws-amplify';
 import styles from './userstyles';
 
 // current user box component, displays user info and log out button
 const CurrentUserBoxComponent: React.FC = () => {
   // gets current logged in user from store
+  const [image, setImage] = React.useState(null);
   const currentUser = useSelector((state: RootStore) => state.auth.user);
   const dispatch = useDispatch();
 
@@ -23,13 +25,26 @@ const CurrentUserBoxComponent: React.FC = () => {
     dispatch(logout());
   };
 
+  const getUserImg = async () => {
+    let filename = `${currentUser?.username}/myimages`;
+    const signUrl: any = await Storage.get(filename);
+    // return signUrl;
+
+    setImage(signUrl);
+  };
+
+  React.useEffect(() => {
+    getUserImg();
+  }, []);
+
   return (
     <SafeAreaView style={styles.androidSafeArea}>
       {/* user profile picture */}
       <Image
-        source={require('../../assets/defaultUserImage.png')}
-        style={styles.userImg}
-        resizeMode='contain'
+        source={{ uri: image as any }}
+        style={{ width: 72, height: 72, borderRadius: 72 / 2 }}
+        // style={styles.userImg}
+        // resizeMode='contain'
       ></Image>
       {/* username and bio */}
       <Text style={styles.usernameText}>@{currentUser?.username}</Text>
@@ -41,7 +56,7 @@ const CurrentUserBoxComponent: React.FC = () => {
             flexDirection: 'row',
             justifyContent: 'center',
             alignItems: 'center',
-            alignContent: 'center'
+            alignContent: 'center',
           }}
         >
           <MaterialCommunityIcons name='logout' size={18} color='#fff' />
