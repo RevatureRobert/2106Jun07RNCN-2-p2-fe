5560a6fc9c1d1f638c5e21f40cfc444eda45bcf8
calebmsword <caleb.m.sword@gmail.com>
2021-07-23 17:28:41 -0400

    caleb refactoring

--- __tests__/ChirpItemComponent.test.js
+++ __tests__/ChirpItemComponent.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import configureStore from 'redux-mock-store';
 import { mount } from 'enzyme'
-import { Provider } from 'react-redux';
 import ChirpItemComponent from '../src/components/chirps/ChirpItemComponent'; 
 import thunk from 'redux-thunk';
 import { testState } from '../src/shared/constants';
@@ -36,46 +35,4 @@ describe('Testing ChirpsComponent', () => {
         expect(wrapper).toBeDefined();
     });
 
-    it('should display the username of they who chirped', () => {
-        // alert(wrapper.debug())
-        // alert(testState.userChirp.username);
-        // alert(
-        //     wrapper.find(
-        //         {testID: 'username-of-chirp'}
-        //         // {children: '@dummyUser'}
-        //     )
-        // );
-        // alert(wrapper.find({testID: 'username-of-chirp'}).text());
-        // expect(
-        //     wrapper.find({testID: 'username-of-chirp'}).text()
-        // ).toBe(`@{testState.userChirp.username}`);
-        // expect(wrapper.text()).toMatch(testState.userChirp.username);
-    });
-
 });
-
-class ErrorBoundary extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { hasError: false };
-    }
-  
-    static getDerivedStateFromError(error) {
-      // Update state so the next render will show the fallback UI.
-      return { hasError: true };
-    }
-  
-    componentDidCatch(error, errorInfo) {
-      // You can also log the error to an error reporting service
-      logErrorToMyService(error, errorInfo);
-    }
-  
-    render() {
-      if (this.state.hasError) {
-        // You can render any custom fallback UI
-        return <h1>Something went wrong.</h1>;
-      }
-  
-      return this.props.children; 
-    }
-  }
