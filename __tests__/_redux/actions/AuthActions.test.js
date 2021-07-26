457dfb8506310a3d4615aaee763a36ea3a58853b
calebmsword <caleb.m.sword@gmail.com>
2021-07-26 16:14:50 -0400

    TEST

---
+++ __tests__/_redux/actions/AuthActions.test.js
@@ -0,0 +1,71 @@
+import {
+    signIn,
+    // signUp,
+    // getVerification,
+    // logout,
+    setError,
+    // setSuccess,
+    // setLoading,
+} from '../../../src/redux/actions/AuthActions';
+import * as reactRedux from 'react-redux';
+import awsAmplify from 'aws-amplify';
+import SET_USER from '../../../src/redux/types/AuthActionsTypes';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { Store } from '../../../src/redux/store/store';
+
+jest.mock('aws-amplify');
+jest.mock('../../../src/redux/actions/AuthActions');
+awsAmplify.Auth.signIn.mockImplementation( (username, password) => {
+    if (username === 'dummyuser' && password === '@Test000') {
+        return Promise.resolve({ 
+            getUsername: () => { 
+                return 'dummyuser'
+            } 
+        });
+    } else {
+        return Promise.reject( new Error('username and password not recognized') );
+    }
+});
+
+let store;
+let mockStore;
+
+describe('testing signIn', () => {
+    const mockSetError = jest.fn();
+    jest.mock('../../../src/redux/actions/AuthActions', () => ({
+        setError: mockSetError,
+    }));
+    
+    const goodSignInData = {
+        username: 'dummyuser',
+        password: '@Test000',
+    }
+
+    const badSignInData = {
+        username: 'doesnotexist',
+        password: '@Test000',
+    }
+
+    const mockOnError = jest.fn(() => { /*no-op*/});
+    
+    beforeEach( () => {
+        mockStore = configureMockStore([thunk]);
+        store = mockStore();
+        console.log(store);
+    });
+
+    it('works', () => {
+        const userData = {
+            username: goodSignInData.username,
+            password: goodSignInData.username,
+            picture: `https://chirps-bucket-for-pics.s3.us-east-2.amazonaws.com/public/${goodSignInData.username}/myimages`
+        };
+        store.dispatch(signIn(goodSignInData, mockOnError)).then( () => {
+            expect(store.getActions().toContain({
+                    type: SET_USER,
+                    payload: userData
+            }));
+        });
+    });
+});
--- jest.config.ts
+++ jest.config.ts
@@ -8,60 +8,56 @@ type configObj = {
 }
 function withExtras(config: configObj){
   config.setupFilesAfterEnv.push('<rootDir>/__tests__/test-setup.js');
-  if ( config.hasOwnProperty('testPathIgnorePatterns') ) {
-    config.testPathIgnorePatterns.push('<rootDir>/__tests__/_componentTests/App.test.js');
-  } else {
-    config.testPathIgnorePatterns = ['<rootDir>/__tests__/_componentTests/App.test.js'];
-  }
-
+  config.testPathIgnorePatterns = [
+      '<rootDir>/__tests__/_components/App.test.js',
+      '<rootDir>/__tests__/_redux/*',
+  ];
   return config;
 }
 
-module.exports = {
-    //==========================================================================
-    // Configure Enzyme (see https://npm.io/package/jest-expo-enzyme)
-    // see link in comment before configObj declaration as well
-    //==========================================================================
-    projects: [
-      withExtras(withEnzyme(require('jest-expo/ios/jest-preset')) as configObj),
-      withExtras(withEnzyme(require('jest-expo/android/jest-preset')) as configObj),
-      // withExtras(withEnzyme(require('jest-expo/web/jest-preset')) as configObj),
-      // {
-      //   transform: {
-      //     '^.+\\.(js|ts|tsx)$': 'babel-jest',
-      //     '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp|ttf|otf|m4v|mov|mp4|mpeg|mpg|webm|aac|aiff|caf|m4a|mp3|wav|html|pdf|obj)$': 'C:\\Users\\Bigmo\\Revature\\2106RNCN-2-p2-fe\\node_modules\\jest-expo\\src\\preset\\assetFileTransformer.js'
-      //   },
-      //   transformIgnorePatterns: [
-      //     'node_modules/(?!(jest-)?react-native|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'  
-      //   ],
-      //   displayName: { name: 'redux', color: 'yellow' },
-      //   testMatch: [
-      //     '**/__tests__/**/*test_.[jt]s?(x)',
-      //   ],
-      //   moduleFileExtensions: [
-      //     'ts',             'tsx',
-      //     'js',             'jsx',
-      //   ],
-      // },
-    ],
 
-    //==========================================================================
-    // configure Jest coverage report
-    //==========================================================================
-    collectCoverage: true,
-    collectCoverageFrom: [
-      './src/**/*.{ts, tsx}',
-      '!./src/redux/axiosConfig.ts',
-      '!./src/redux/store/store.ts',
-    ],
-    coveragePathIgnorePatterns: [
-      'axiosConfig',
-      'redux/store',
-    ],
-    coverageDirectory: 'coverage',
-    coverageThreshold: {
-      global: {
-        statements: 30,
-      }
-    },
+//configure redux tests
+const reduxConfig = {
+  transform: {
+    '^.+\\.(js|ts|tsx)$': 'babel-jest',
+    '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp|ttf|otf|m4v|mov|mp4|mpeg|mpg|webm|aac|aiff|caf|m4a|mp3|wav|html|pdf|obj)$': 'C:\\Users\\Bigmo\\Revature\\2106RNCN-2-p2-fe\\node_modules\\jest-expo\\src\\preset\\assetFileTransformer.js'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'  
+  ],
+  displayName: { name: 'redux', color: 'yellow' },
+  testMatch: [
+    '**/__tests__/**/*test.[jt]s?(x)',
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/__tests__/_componentTests/App.test.js',
+    '<rootDir>/__tests__/_components/*'
+  ],
+  moduleFileExtensions: [
+    'ts',             'tsx',
+    'js',             'jsx',
+  ],
+}
+
+
+module.exports = {
+  projects: [
+    withExtras(withEnzyme(require('jest-expo/ios/jest-preset')) as configObj),
+    withExtras(withEnzyme(require('jest-expo/android/jest-preset')) as configObj),
+    // withExtras(withEnzyme(require('jest-expo/web/jest-preset')) as configObj),
+    // reduxConfig,
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    './src/**/*.{ts, tsx}',
+    '!./src/redux/axiosConfig.ts',
+    '!./src/redux/**/*.{ts, tsx}',
+    '!./src/components/semantic/ImageForWeb/types.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      statements: 30,
+    }
+  },
 };
