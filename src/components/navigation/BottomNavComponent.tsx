7bdc57f9f390755b6ca62e97af809c9a7f672731
Red Oral <redmunozoral@gmail.com>
2021-07-26 14:28:29 -0700

    Fixed bottom navigation.

--- src/components/navigation/BottomNavComponent.tsx
+++ src/components/navigation/BottomNavComponent.tsx
@@ -37,6 +37,21 @@ const BottomNavComponent = () => {
         },
       }}
     >
+      {/* all chirps feed */}
+      <Tab.Screen
+        name='Feed'
+        component={ChirpsComponent}
+        options={{
+          tabBarLabel: 'Feed',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name='home-outline'
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
       <Tab.Screen
         name='Search'
         component={SearchView}
@@ -71,22 +86,6 @@ const BottomNavComponent = () => {
           ),
         }}
       />
-      {/* all chirps feed */}
-      <Tab.Screen
-        name='Feed'
-        component={ChirpsComponent}
-        options={{
-          tabBarLabel: 'Feed',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons
-              name='home-outline'
-              color={color}
-              size={size}
-            />
-          )
-        }}
-      />
-      
       {/* user settings */}
       <Tab.Screen
         name='Settings'
