26f968997799c1b9d76973454f8d19566702764a
calebmsword <caleb.m.sword@gmail.com>
2021-07-30 14:45:41 -0400

    weird typos in jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -11,15 +11,14 @@ type configObj = {
 
 function withExtras(config: configObj){
   config.setupFilesAfterEnv.push('<rootDir>/__tests__/test-setup.js');
-  // config.testMatch = ['<rootDir>/__tests__/_components/addchirp/ImageUploadComponent.test.js',]
   config.testPathIgnorePatterns = ['<rootDir>/__tests__/_shared/functions.test.js',];
   return config;
 }
 
 const testFunctions = {
   transform: {
-  '^.+\\.(js|ts|tsx)$': 'babel-jest',
-  '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp|ttf|otf|m4v|mov|mp4|mpeg|mpg|webm|aac|aiff|caf|m4a|mp3|wav|html|pdf|obj)$': 'C:\\Users\\Bigmo\\Revature\\2106RNCN-2-p2-fe\\node_modules\\jest-expo\\src\\preset\\assetFileTransformer.js'
+    '^.+\\.(js|ts|tsx)$': 'babel-jest',
+    '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp|ttf|otf|m4v|mov|mp4|mpeg|mpg|webm|aac|aiff|caf|m4a|mp3|wav|html|pdf|obj)$': 'C:\\Users\\Bigmo\\Revature\\2106RNCN-2-p2-fe\\node_modules\\jest-expo\\src\\preset\\assetFileTransformer.js'
   },
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'  
@@ -36,7 +35,7 @@ const testFunctions = {
 
 module.exports = {
   projects: [
-    // withExtras(withEnzyme(require('jest-expo/ios/jest-preset')) as configObj),
+    withExtras(withEnzyme(require('jest-expo/ios/jest-preset')) as configObj),
     withExtras(withEnzyme(require('jest-expo/android/jest-preset')) as configObj),
     testFunctions,
   ],
