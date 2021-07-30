fc777ce4aa54e73d338b53a23420e1c2ff1d9a9b
calebmsword <caleb.m.sword@gmail.com>
2021-07-30 14:42:06 -0400

    tests, final, I mean it this time

--- __tests__/_components/addchirp/ImageUploadComponent.test.js
+++ __tests__/_components/addchirp/ImageUploadComponent.test.js
@@ -13,7 +13,7 @@ const component = () => {
     return (<ImageUploadComponent setImageURL={ () => {/*no-op*/} }/>);
 }
 
-it('displays icon that responds to press even', async () => {
+it('displays icon that responds to press event', async () => {
     //mock libraries used by eventHandler 
     jest.spyOn(Storage, 'get').mockImplementation( () => {
         return Promise.resolve( {} );
@@ -29,6 +29,37 @@ it('displays icon that responds to press even', async () => {
         return Promise.resolve( {blob: func} );
     });
 
+    const wrapper = mount(nestedHell(testState, component));
+    let wrap = wrapper.find(TouchableOpacity);
+    if (wrap.length > 1) {
+        wrap = wrap.last();
+    }
+
+    const eventHandler = jest.spyOn(wrap.props(), 'onPress');
+    wrap.props().onPress();
+    expect(eventHandler).toHaveBeenCalled();
+});
+
+it('event handler responds appropriately if you don\'t have proper permissions', async () => {
+    //mock libraries used by eventHandler 
+    jest.spyOn(Storage, 'get').mockImplementation( () => {
+        return Promise.resolve( {} );
+    });
+    jest.spyOn(Storage, 'put').mockImplementation( () => {
+        return Promise.resolve( {uri: ''} );
+    });
+    jest.spyOn(ImagePicker, 'launchImageLibraryAsync').mockImplementation( () => {
+        return ({ uri: '' })
+    });
+    jest.spyOn(global, 'fetch').mockImplementation( () => {
+        const func = () => { return ( {} ) };
+        return Promise.resolve( {blob: func} );
+    });
+    jest.spyOn(ImagePicker, 'requestMediaLibraryPermissionsAsync')
+        .mockResolvedValue( {status: "not granted"} );
+    jest.spyOn(ImagePicker, 'requestCameraPermissionsAsync')
+        .mockResolvedValue( {status: "not granted"} );
+    
     const wrapper = mount(nestedHell(testState, component));
     let wrap = wrapper.find(TouchableOpacity);
     if (wrap.length > 1) {
